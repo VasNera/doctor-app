7a55d0a240b7dbd951f312201d48a922dba25b2c
Vas. Neratzis <vas.neratzis@gmail.com>
2026-06-27 17:03:23 +0300

    feat: add doctor and patient entities

---
+++ doctor-app/src/main/java/gr/aueb/cf/doctor_app/model/Doctor.java
@@ -0,0 +1,84 @@
+package gr.aueb.cf.doctor_app.model;
+
+
+import gr.aueb.cf.doctor_app.model.enums.Specialty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Time;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "doctors")
+public class Doctor extends AbstractEntity{
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+
+    private String lastname;
+
+    @Column(unique = true,nullable = false, updatable = false, columnDefinition = "BINARY(16)")
+    private UUID uuid = UUID.randomUUID();
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "licence_number", nullable = false, unique = true)
+    private String licenceNumber;
+
+    @Enumerated(EnumType.STRING)
+    private Specialty specialty;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+
+    @OneToMany(mappedBy = "doctor",fetch = FetchType.LAZY)
+    private Set<TimeSlot> timeSlots = new HashSet<>();
+
+
+    public void addTimeSlot(TimeSlot timeSlot){
+        timeSlots.add(timeSlot);
+        timeSlot.setDoctor(this);
+    }
+
+    public void removeTimeSlot(TimeSlot timeSlot){
+        timeSlots.remove(timeSlot);
+        timeSlot.setDoctor(null);
+
+    }
+
+
+    public void addUser(User user){
+        this.user = user;
+        user.setDoctor(this);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (!(other instanceof Doctor doctor)) return false;
+        return Objects.equals(getUuid(), doctor.getUuid());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getUuid());
+    }
+}
---
+++ doctor-app/src/main/java/gr/aueb/cf/doctor_app/model/Patient.java
@@ -0,0 +1,83 @@
+package gr.aueb.cf.doctor_app.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "patients")
+public class Patient extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+
+    private String lastname;
+
+    @Column(unique = true,nullable = false, updatable = false, columnDefinition = "BINARY(16)")
+    private UUID uuid = UUID.randomUUID();
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String amka;
+
+    @Column(name = "phone_number", nullable = false)
+   private String phoneNumber;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @OneToMany(mappedBy = "patient", fetch = FetchType.LAZY)
+    private Set<Appointment> appointments  = new HashSet<>();
+
+
+    public void addAppointment(Appointment appointment){
+        if (appointment != null) {
+            appointments.add(appointment);
+            appointment.setPatient(this);
+        }
+    }
+
+    public void removeAppointment(Appointment appointment){
+        appointments.remove(appointment);
+        appointment.setPatient(null);
+    }
+
+
+    public void addUser(User user){
+        this.user = user;
+        user.setPatient(this);
+
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if(this == other) return true;
+        if (!(other instanceof Patient patient)) return false;
+        return Objects.equals(getUuid(), patient.getUuid());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getUuid());
+    }
+}
