c5bcf9f8f3d8adb8c7c0b528d87620d44e69e231
Vas. Neratzis <vas.neratzis@gmail.com>
2026-06-28 22:00:09 +0300

    feat: add Appointment and Timeslot models

---
+++ doctor-app/src/main/java/gr/aueb/cf/doctor_app/model/Appointment.java
@@ -0,0 +1,55 @@
+package gr.aueb.cf.doctor_app.model;
+
+
+import gr.aueb.cf.doctor_app.model.enums.AppointmentStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Table(name = "appointments")
+public class Appointment extends AbstractEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true, updatable = false, columnDefinition = "BINARY(16)")
+    private UUID uuid = UUID.randomUUID();
+
+    private String reason;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AppointmentStatus appointmentStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id",nullable = false)
+    private Patient patient;
+
+    @OneToOne
+    @JoinColumn(name = "timeslot_id", nullable = false, unique = true)
+    private TimeSlot timeSlot;
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (!(other instanceof Appointment appointment)) return false;
+        return Objects.equals(getUuid(), appointment.getUuid());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getUuid());
+    }
+}
+
---
+++ doctor-app/src/main/java/gr/aueb/cf/doctor_app/model/TimeSlot.java
@@ -0,0 +1,63 @@
+package gr.aueb.cf.doctor_app.model;
+
+
+import gr.aueb.cf.doctor_app.model.enums.TimeSlotsStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "timeslots")
+public class TimeSlot extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalTime endTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TimeSlotsStatus timeSlotsStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private Doctor doctor;
+
+    @OneToOne(mappedBy ="timeSlot", fetch = FetchType.LAZY)
+    private Appointment appointment;
+
+    public void addDoctor(Doctor doctor){
+        this.doctor = doctor;
+        doctor.getTimeSlots().add(this);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (!(other instanceof TimeSlot timeSlot)) return false;
+        return id != null && id.equals(timeSlot.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
