963249284a49080decaffe72da1384cb767cbfca
Vas. Neratzis <vas.neratzis@gmail.com>
2026-06-30 18:22:51 +0300

    feat: add Specialty as enum

---
+++ doctor-app/src/main/java/gr/aueb/cf/doctor_app/model/enums/Specialty.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.doctor_app.model.enums;
+
+public enum Specialty {
+    CARDIOLOGY,
+    DERMATOLOGY,
+    ORTHOPEDICS,
+    NEUROLOGY,
+    PEDIATRICS,
+    GYNECOLOGY,
+    UROLOGY,
+    GASTROENTEROLOGY,
+    PSYCHIATRY
+}
