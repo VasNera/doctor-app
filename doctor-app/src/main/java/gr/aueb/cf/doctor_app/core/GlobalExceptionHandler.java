3345377616d1eb97f52964144ef0b76ff2f16a56
Vas. Neratzis <vas.neratzis@gmail.com>
2026-07-01 21:34:27 +0300

    feat: add GlobalExceptionHandler

---
+++ doctor-app/src/main/java/gr/aueb/cf/doctor_app/core/GlobalExceptionHandler.java
@@ -0,0 +1,93 @@
+package gr.aueb.cf.doctor_app.core;
+
+import gr.aueb.cf.doctor_app.core.exceptions.*;
+import gr.aueb.cf.doctor_app.dto.ErrorResponseDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleEntityAlreadyExistsException(EntityAlreadyExistsException e){
+        log.warn("Entity already exists. Message ={}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorResponseDTO(e.getCode(), e.getMessage()));
+
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleEntityNotFoundException(EntityNotFoundException e){
+        log.warn("Entity not Found. Message={}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponseDTO(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidArgumentException.class)
+    public ResponseEntity<ErrorResponseDTO> handleInvalidArgumentException(InvalidArgumentException e){
+        log.warn("Invalid argument. Message ={}" , e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponseDTO(e.getCode(), e.getMessage()));
+
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleInvalidCredentialsException(InvalidCredentialsException e){
+        log.warn("Invalid Credentials. Message ={}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponseDTO(e.getCode(), e.getMessage()));
+        }
+
+        @ExceptionHandler(TimeSlotNotAvailableException.class)
+        public ResponseEntity<ErrorResponseDTO> handleTimeSlotNotAvailableException(TimeSlotNotAvailableException e){
+        log.warn("Timeslot not Available. Message ={}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorResponseDTO(e.getCode(), e.getMessage()));
+
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponseDTO> handleAuthenticationException(AuthenticationException e,
+                                                                          HttpServletRequest request){
+        log.warn("Failed login for IP={}", request.getRemoteAddr());
+
+        String errorCode = switch (e) {
+            case BadCredentialsException ex -> "INVALID_CREDENTIALS";
+            case DisabledException ex -> "ACCOUNT_DISABLED";
+            case LockedException ex -> "ACCOUNT_LOCKED";
+            case AccountExpiredException ex -> "ACCOUNT_EXPIRED";
+            case CredentialsExpiredException ex -> "CREDENTIALS_EXPIRED";
+            default -> "AUTHENTICATION_ERROR";
+        };
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)      // 401
+                .body(new ErrorResponseDTO(errorCode, e.getMessage()));
+    }
+
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleGenericException(Exception e){
+        log.error("Unexpected error. Message ={}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDTO("INTERNAL_SERVER_ERROR", "An unexpected error occurred."));
+    }
+
+}
