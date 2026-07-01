4efd321c401af89ac8810a1211d0c144b37f12a2
Vas. Neratzis <vas.neratzis@gmail.com>
2026-07-01 19:50:54 +0300

    fix: add entityGraphPath

--- doctor-app/src/main/java/gr/aueb/cf/doctor_app/repository/UserRepository.java
+++ doctor-app/src/main/java/gr/aueb/cf/doctor_app/repository/UserRepository.java
@@ -3,12 +3,14 @@ package gr.aueb.cf.doctor_app.repository;
 import gr.aueb.cf.doctor_app.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @EntityGraph(attributePaths = {"role", "role.capabilities"})
     Optional<User> findByUsername(String username);
 
     Page<User> findAllByDeletedFalse(Pageable pageable);
