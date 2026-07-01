c30444deb6de626ef6019a71560807d13d9583a4
Vas. Neratzis <vas.neratzis@gmail.com>
2026-07-01 21:30:33 +0300

    feat: add findByName in roleRepository

--- doctor-app/src/main/java/gr/aueb/cf/doctor_app/repository/RoleRepository.java
+++ doctor-app/src/main/java/gr/aueb/cf/doctor_app/repository/RoleRepository.java
@@ -5,8 +5,11 @@ import gr.aueb.cf.doctor_app.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     List<Role> findAllByOrderByNameAsc();
+
+    Optional<Role> findByName(String name);
 }
