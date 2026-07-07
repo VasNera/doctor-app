01ccc2637224cebd6f745a6d0f9fab2ce8822939
Vas. Neratzis <vas.neratzis@gmail.com>
2026-07-07 21:54:49 +0300

    feat: add method at GlobalExceptionHandler

--- doctor-app/src/main/java/gr/aueb/cf/doctor_app/core/GlobalExceptionHandler.java
+++ doctor-app/src/main/java/gr/aueb/cf/doctor_app/core/GlobalExceptionHandler.java
@@ -2,21 +2,39 @@ package gr.aueb.cf.doctor_app.core;
 
 import gr.aueb.cf.doctor_app.core.exceptions.*;
 import gr.aueb.cf.doctor_app.dto.ErrorResponseDTO;
+import gr.aueb.cf.doctor_app.dto.ValidationErrorResponseDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ValidationErrorResponseDTO> handleValidationException(ValidationException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return new ResponseEntity<>(
+                new ValidationErrorResponseDTO(e.getCode(), e.getMessage(), errors),
+                HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(EntityAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDTO> handleEntityAlreadyExistsException(EntityAlreadyExistsException e){
